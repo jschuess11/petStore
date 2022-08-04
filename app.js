5a650f74f8cf2322c38aa17edc128f04f698c137
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-08-04 16:21:00 -0400

    store js

---
+++ app.js
@@ -0,0 +1,58 @@
+var shop = [
+    {
+      title: 'Yogi Bear',
+      image: './imgs/yogi.png',
+      price: '$15,000'
+      description: 'Not good around unattended food'
+    },
+    {
+      title: 'Santa\'s Little Helper',
+      image: './imgs/santashelper.png',
+      price: '$25,000'
+      description: 
+    },
+    {
+      title: 'SPECIAL: 2 Stupid Dogs for price of 1',
+      image: './imgs/2stupiddogs.png',
+      price: '$1000'
+      description:
+    },
+    {
+      title: 'Tom',
+      image: './imgs/tom.png',
+      price: '$20,000'
+      description:
+    },
+    {
+      title: 'Dino',
+      image: './imgs/dino.png',
+      price: '$12,000'
+      description:
+    },
+    {
+      title: 'Bugs Bunny',
+      image: './imgs/bugsbunny.png',
+      price: '$50,000'
+      description
+    },
+    {
+      title: 'Crush',
+      image: '/imgs/Crush.png',
+      price: '$30,000'
+      description
+    },
+    
+    ]
+    
+    var postHTML = " "
+  
+  
+    for (var i=0; i < shop.length; i++){
+        var heading = '<div class="product ' + '"><span><h5>' + shop[i].title + '</h5>'
+        var image = '<img src="' + shop[i].image + '"/'
+        var price = '<p> $' + shop[i].price + '</p></span>'
+        var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+        var concatThis = heading + image + price + description;
+        postHTML = postHTML + concatThis
+    }
+    document.getElementById('market').innerHTML = postHTML
